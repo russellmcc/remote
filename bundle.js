c0e36cf71645ac3e8a76af128a30bc9806909b4c
Jenna Fizel <jftesser@gmail.com>
2015-07-17 14:39:30 -0400

    fixed hue bridge ip

--- bundle.js
+++ bundle.js
@@ -21027,12 +21027,12 @@ var Vec2 = require('vec2');
 
 
 var hueOff = function() {
-  xhr({url: 'http://10.0.1.12/api/newdeveloper/lights/3'})
+  xhr({url: 'http://10.0.1.23/api/newdeveloper/lights/3'})
       .then(JSON.parse.bind(JSON))
       .then(function(resp){
         xhr({
           verb: 'PUT',
-          url: 'http://10.0.1.12/api/newdeveloper/lights/3/state',
+          url: 'http://10.0.1.23/api/newdeveloper/lights/3/state',
           data: JSON.stringify({
             on: false
           })
